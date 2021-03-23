87d490e4139771ff80aed764f06020dedf54e71f
Friederike Moeser <friederike.moeser@gmail.com>
2021-03-23 21:25:59 +0100

    Add storage

--- create.js
+++ create.js
@@ -1,16 +1,48 @@
-// Log form input
+import { pushSection } from './src/lib/db.js'
 
-const form = document.querySelector('.form')
-const inputData = []
+const form = document.querySelector('form')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
-  const newCreation = {
-    questionInput: form['question'].value,
-    answerInput: form['answer'].value,
-    tagsInput: form['tags'].value,
+  // turn input inside the tag text area into an array
+
+  const questionInput = form['question'].value
+  const answerInput = form['answer'].value
+  const tagsInput = form['tags']
+  const tagsInputArray = tagsStringToArray(tagsInput.value)
+
+  const input = {
+    question: questionInput,
+    answer: answerInput,
+    tags: tagsInputArray,
   }
-  inputData.push(newCreation)
-  console.log(inputData)
+
+  pushSection(input)
+
+  console.log(input)
   form.reset()
+  alert('Question created! 🎉')
 })
+
+function tagsStringToArray(tagsString) {
+  const itemsArray = tagsString.split(',')
+
+  return itemsArray
+}
+
+// Log form input
+
+// const form = document.querySelector('.form')
+// const inputData = []
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault()
+//   const newCreation = {
+//     questionInput: form['question'].value,
+//     answerInput: form['answer'].value,
+//     tagsInput: form['tags'].value,
+//   }
+//   inputData.push(newCreation)
+//   console.log(inputData)
+//   form.reset()
+// })
--- quiz.js
+++ quiz.js
@@ -1,7 +1,8 @@
-import { data } from './src/lib/db.js'
+import { getSection } from './src/lib/db.js'
 import createCard from './src/lib/card.js'
 
 const content = document.querySelector('.grid-content')
+const data = getSection()
 
 for (let i = 0; i < data.length; i++) {
   const cardSection = createCard(data[i])
